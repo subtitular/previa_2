16b09e1457c078de9256d0e7a366bc41c5a2a7fb
Oscar Peña <o_a_p_c@hotmail.com>
2022-09-13 13:31:58 -0500

    Funcionando ok

--- src/main/java/com/grupo15/previa_2/Previa_2.java
+++ src/main/java/com/grupo15/previa_2/Previa_2.java
@@ -5,13 +5,54 @@
 
 package com.grupo15.previa_2;
 
+import com.grupo15.Exceptions.HandledException;
+import java.text.MessageFormat;
+import java.util.Scanner;
+
 /**
  *
  * @author tecnologia7
  */
 public class Previa_2 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final String OUT_OF_RANGE_EXCEPTION = "EXCEPCIÓN FUERA DE RANGO";
+    private static final double porcentajeAumento=10.0;
+    private static final double porcentajeRebaja=10.0;
+    private static int valorP;
+    
+    public static void main(String[] args) throws HandledException {
+        int valor;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Digite el valor de la libra a las 11:00AM (p)");
+        try {
+            valor=leer.nextInt();
+            double valoralas9 = regresarAumento(valor);
+            double valorala1 = rebajar(valor);
+            System.out.println(MessageFormat.format("Cuando a las 11:00 la carne vale {0} a las 9:00AM valía {1}",valor,valoralas9));
+            System.out.println(MessageFormat.format("Cuando a las 11:00 la carne vale {0} a la 1:00PM valdrá {1}",valor, valorala1));
+        
+        } catch (HandledException e) {
+            System.out.println("Code: "+e.getCode()+" Exception Message : "+e.getMessage());
+        }
     }
+    
+    private static double regresarAumento(int valor) throws HandledException{
+        if(valor < 0 ) {
+            throw new HandledException(OUT_OF_RANGE_EXCEPTION,MessageFormat.format("El año {0} no puede ser negativo",valor));
+        }
+        return valor/((100 + porcentajeAumento)/100);
+    }
+    private static double aumentar(int valor) throws HandledException{
+        if(valor < 0 ) {
+            throw new HandledException(OUT_OF_RANGE_EXCEPTION,MessageFormat.format("El año {0} no puede ser negativo",valor));
+        }
+        return valor * porcentajeAumento;
+    }
+    private static double rebajar(int valor) throws HandledException{
+        if(valor < 0 ) {
+            throw new HandledException(OUT_OF_RANGE_EXCEPTION,MessageFormat.format("El año {0} no puede ser negativo",valor));
+        }
+        return valor * (100 - porcentajeRebaja)/100;
+    }
+    
 }
+
